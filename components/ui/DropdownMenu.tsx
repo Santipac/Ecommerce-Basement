2ca863ed0112e92b62329e07103cb937243aba6b
Santi <santiagopacini@gmail.com>
2022-11-18 11:35:01 -0300

    new Dropdown menu component and conditional render to orders

--- components/ui/DropdownMenu.tsx
+++ components/ui/DropdownMenu.tsx
@@ -1,16 +1,12 @@
-import {
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-  Button,
-} from '@material-tailwind/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useUserStore } from '../../hooks/useUserStore';
 
+import React, { useState } from 'react';
+
 export const DropdownMenu = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const router = useRouter();
   const { startSignOutUser, photoURL, displayName, uid } = useUserStore();
 
@@ -19,34 +15,40 @@ export const DropdownMenu = () => {
     startSignOutUser();
   };
 
-  return (
-    <Menu>
-      <MenuHandler>
-        <Button
-          style={{
-            backgroundColor: 'transparent',
-            boxShadow: 'none',
-            borderRadius: '100%',
-            padding: 0,
-          }}
-        >
-          <Image
-            src={photoURL ?? ''}
-            height={40}
-            width={40}
-            className="rounded-full object-cover cursor-pointer"
-            alt="Avatar User"
-          />
-        </Button>
-      </MenuHandler>
-      <MenuList>
-        <MenuItem onClick={() => router.push('/')}>Home</MenuItem>
-        <MenuItem>
-          <Link href={`/orders/${uid}`}>My Orders</Link>
-        </MenuItem>
+  const onOpenMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-        <MenuItem onClick={() => signOut()}>Sign Out</MenuItem>
-      </MenuList>
-    </Menu>
+  return (
+    <div>
+      <button className="rounded-full relative" onClick={() => onOpenMenu()}>
+        <Image
+          src={photoURL ?? ''}
+          height={40}
+          width={40}
+          className="rounded-full object-cover cursor-pointer"
+          alt="Avatar User"
+        />
+      </button>
+      {isOpen && (
+        <div className="h-36 w-36 absolute right-12 z-4 mt-1 bg-black border-2 border-gray-600 text-white rounded-md flex flex-col space-y-1.5">
+          <p className="px-2 pt-2 text-center">{displayName?.split(' ')[0]}</p>
+          <div className="w-full h-[1.2px] bg-gray-600"></div>
+          <Link href="/" className="px-2">
+            Home
+          </Link>
+          <Link href={`/orders/${uid}`} className="px-2">
+            Orders
+          </Link>
+          <div className="w-full h-[1.2px] bg-gray-600"></div>
+          <button
+            onClick={() => signOut()}
+            className="px-2 text-indigo-500 text-left"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
